fb9d66667f610ca7565a884afe297f4e0c6b2468
Josh <joshua.boycehyland15@gmail.com>
2023-12-05 11:03:56 +0000

    changes data base and added to get data

--- Assignment3/schema.sql
+++ Assignment3/schema.sql
@@ -1,10 +1,27 @@
-create database BlackJackDB
+create database BlackJackDB;
 
-create table gameStats(
+grant all on BlackJackDB.* to 'josh'@'localhost' identified by 'password';
+
+exit 
+
+mysql -u josh -p BlackJackDB
+
+
+create table Games(
 gamertag varchar(32) not null,
 outcome varchar(4) not null,
-hits int(2) not null,
-bust varchar(3) not null,
-21s varchar(3) not null,
-blackjack varchar(3) not null);
+21s varchar(3) not null);
+
+
+
+create table GameStats(
+gamertag varchar(32) not null, 
+winrate int(2) not null, 
+HighestWinStreak int(3) not null, 
+21s int(3) not null);
+
+create table Players(
+    gamertag varchar(32) not null);
+
+source players.sql; 
     
